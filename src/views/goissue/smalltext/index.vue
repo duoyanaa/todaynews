d80de6e0f8a29d2b381fff5f40578b19b6b59824
duoyanaa <1440287390@qq.com>
2020-10-08 17:17:12 +0800

    1

--- src/views/goissue/smalltext/index.vue
+++ src/views/goissue/smalltext/index.vue
@@ -79,7 +79,9 @@ export default {
     },
   },
   //生命周期 - 创建完成（可以访问当前this实例）
-  created() {},
+  created() {
+    alert(111)
+  },
   //生命周期 - 挂载完成（可以访问DOM元素）
   mounted() {},
   beforeCreate() {},
